60d51fe6a9620793c5c3574ad1738a00f75d8ed2
Neoxx <pavlackyt@gmail.com>
2020-04-05 17:58:37 +0200

    Added decompiled examples to null coalesting assignment

--- Examples/Null-CoalescingAssignment/Numbers.cs
+++ Examples/Null-CoalescingAssignment/Numbers.cs
@@ -11,6 +11,23 @@ namespace Examples
       numbers.Add(newValue ??= 0);
 
       Console.WriteLine($"Added value: {newValue}");
+
+      //(numbers ?? (numbers = new List<int>())).Add(5);
+      //ICollection<int> obj = numbers;
+      //int valueOrDefault = newValue.GetValueOrDefault();
+      //int item;
+      //if (!newValue.HasValue)
+      //{
+      //  valueOrDefault = 0;
+      //  newValue = valueOrDefault;
+      //  item = valueOrDefault;
+      //}
+      //else
+      //{
+      //  item = valueOrDefault;
+      //}
+      //obj.Add(item);
+      //Console.WriteLine(string.Format("Added value: {0}", newValue));
     }
 
     private void OldSchoolWay(ref ICollection<int> numbers, int? newValue)
@@ -19,6 +36,16 @@ namespace Examples
 
       numbers.Add((newValue = newValue ?? 0).Value);
       Console.WriteLine($"Added value: {newValue}");
+
+      //ICollection<int> obj = numbers ?? new List<int>();
+      //ICollection<int> collection = obj;
+      //numbers = obj;
+      //collection.Add(5);
+      //ICollection<int> obj2 = numbers;
+      //newValue = newValue.GetValueOrDefault();
+      //int? num = newValue;
+      //obj2.Add(num.Value);
+      //Console.WriteLine(string.Format("Added value: {0}", newValue));
     }
   }
 }
